de73f857b55d153d20229222b3c484f9afee1aaa
Vikctar <vikc.steve@gmail.com>
2018-01-11 15:43:02 +0300

    Add BottonNavigationView to layout and set it up in MainActivity

--- app/src/main/java/com/vikcandroid/bottomnav/MainActivity.java
+++ app/src/main/java/com/vikcandroid/bottomnav/MainActivity.java
@@ -1,13 +1,51 @@
 package com.vikcandroid.bottomnav;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBar actionBar;
+
+    @BindView(R.id.navigation)
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.title_shop);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragment;
+                    switch (item.getItemId()) {
+                        case R.id.navigation_shop:
+                            actionBar.setTitle(R.string.title_shop);
+                            return true;
+                        case R.id.navigation_cart:
+                            actionBar.setTitle(R.string.title_cart);
+                            return true;
+                        case R.id.navigation_profile:
+                            actionBar.setTitle(R.string.title_profile);
+                    }
+                    return false;
+                }
+            };
 }
